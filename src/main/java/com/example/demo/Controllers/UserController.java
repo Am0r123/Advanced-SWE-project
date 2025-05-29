a737f3ded52aa697477f5994c5a630c9fc4e9af2
Omar-Riad04 <omerriad69@yahoo.com>
2025-05-29 03:02:49 +0300

    Add files via upload

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controllers;
+package com.example.Quiz.demo.controllers;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,65 +7,66 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.example.demo.models.User;
-import com.example.demo.models.Admin;
-import com.example.demo.repostitories.UserRepository;
-import com.example.demo.repostitories.adminrepository;
+import com.example.Quiz.demo.models.User;
+import com.example.Quiz.demo.repositories.UserRepository;
+import com.example.Quiz.demo.repositories.adminrepository;
+import com.example.Quiz.demo.models.Admin;
 
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.web.bind.annotation.PathVariable;
+
+
+import com.example.Quiz.demo.services.UserService;
+
 import jakarta.servlet.http.HttpSession;
+
+
 
 @RestController
 @RequestMapping("/User")
 public class UserController {
 
     @Autowired
+    private UserService userService;
+    @Autowired
     private UserRepository userRepository;
     @Autowired
     private adminrepository adminrepository;
 
-    @GetMapping("")
-    public ModelAndView viewTaskPage() {
-        return new ModelAndView("Issue.html");
-    }
+
+
+
+
     @GetMapping("Register")
    public ModelAndView addUser(@RequestParam(value = "success", required = false) String success,
    @RequestParam(value = "error", required = false) String error) {
-        ModelAndView mav = new ModelAndView("Register.html");
-        if (success != null) {
-            mav.addObject("success", success);
-        }
-    
-        if (error != null) {
-            mav.addObject("error", error);
-        }
-        User newUser = new User();
-        mav.addObject("user", newUser);
-        return mav;
-    }
-
-    @PostMapping("Register")
-    public String saveUser(@ModelAttribute User user) {
-
-        User existingUser = this.userRepository.findByUsername(user.getUsername());
-        if (existingUser != null) {
-            return "<script>window.location.href='/User/Register?error=Username Already Exists';</script>";    
-        }
-
-        String encodedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12));
-        user.setPassword(encodedPassword);
-        this.userRepository.save(user);
+    ModelAndView mav = new ModelAndView("Register.html");
+            if (success != null) {
+                mav.addObject("success", success);
+            }
         
-        return "<script>window.location.href='/home?success=Successfully Registered';</script>";
-    }
+            if (error != null) {
+                mav.addObject("error", error);
+            }
+            User newUser = new User();
+            mav.addObject("user", newUser);
+            return mav;
+}
 
-    @GetMapping("Login")
-    public ModelAndView Login(@RequestParam(value = "success", required = false) String success,
+   @PostMapping("Register")
+   public String saveUser(@ModelAttribute User user) {
+
+
+
+    String encodedPassword = BCrypt.hashpw(user.getPassword(),BCrypt.gensalt(12));
+    user.setPassword(encodedPassword);
+    this.userService.save(user);
+        return "<script>window.location.href='/home?success=Successfully Registered';</script>";
+    
+}
+@GetMapping("Login")
+public ModelAndView Login(@RequestParam(value = "success", required = false) String success,
     @RequestParam(value = "error", required = false) String error) {
-        ModelAndView mav = new ModelAndView("Login");
+    ModelAndView mav = new ModelAndView("Login");
         if (success != null) {
             mav.addObject("success", success);
         }
@@ -76,16 +77,26 @@ public class UserController {
         User newUser = new User();
         mav.addObject("user", newUser);
         return mav;
-    }
+}
 
     @PostMapping("Login")
     public String LoginProcess(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session) {
+        System.out.println("Login attempt: " + username);
+
         User dbUser = this.userRepository.findByUsername(username);
-        
+
         if (dbUser != null) {
+            System.out.println("Login attempt: " + dbUser);
+            System.out.println("Login attempt: " + password);
+            System.out.println("Login attempt: " + password);
+
             Boolean isPasswordMatched = BCrypt.checkpw(password, dbUser.getPassword());
             if (isPasswordMatched) {
+                System.out.println("Login attempt: " + dbUser);
+                
+
                 session.setAttribute("isLoggedIn", true);
+                session.setAttribute("UserId", dbUser.getId());
                 return "<script>window.location.href='/home?success=Welcome " + dbUser.getName() + "';</script>";
             }
         }
@@ -102,3 +113,4 @@ public class UserController {
         return "<script>window.location.href='/User/Login?error=Invalid Username or Password';</script>";
     }    
 }
+
