e02f091a2fa69430ac075c897c300984fff15daa
Mohsen-Samer <mohsen2207017@miuegypt.edu.eg>
2025-05-29 03:00:02 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/models/Event (1).java
@@ -0,0 +1,50 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity//bt3mel table ll event fe database
+public class Event {
+    @Id//unique identifier 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//increamented value a7da wa7da badal auto 
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDate eventDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+}
